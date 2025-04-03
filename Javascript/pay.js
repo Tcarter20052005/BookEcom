9a9edc71a146dd2892a6560a41fa1d3f553306e1
Thomas Carter <36308460+Tcarter20052005@users.noreply.github.com>
2025-04-03 15:09:06 +0100

    Form changes
    Javascript tweeks

--- Javascript/pay.js
+++ Javascript/pay.js
@@ -10,7 +10,7 @@ for (let i = 1; i <=12; i++) {
 
 // Populate Expiry Year
 const yearSelect = document.getElementById("expyear");
-const currentYear = new Date().getFullYear();
+const currentYear = new Date().getFullYear()-2;
 for (let i = 0; i < 10; i++) {
     let option = document.createElement("option");
     option.value = currentYear + i;
@@ -40,10 +40,14 @@ document.getElementById("pay-form"). addEventListener("submit", async function(e
         HideErrorTXT();
         
         const cardNum = document.getElementById("cardNum").value.trim();
-        alert(cardNum.toString().slice(0, 2))
         const expMon = document.getElementById("expmon").value;
         const expYear = document.getElementById("expyear").value;
         const CCV = document.getElementById("seCode").value.trim();
+
+        // Check if not a number
+        if (isNaN(cardNum) || isNaN(expMon) || isNaN(expYear) || isNaN(CCV)) {
+            throw new Error("Incorrect Data Type entered");
+        }
 
         // Form Validation - Step 1 -- Card Number Length Check
         const cardNumLen = cardNum.toString().length;
@@ -57,9 +61,21 @@ document.getElementById("pay-form"). addEventListener("submit", async function(e
             throw new Error("Invalid MasterCard number");
         }
 
-        // Form Validation - Step 3
+        // Form Validation - Step 3 -- Card Expiry Check
+        const checkYear = new Date().getFullYear();
+        const checkMonth = new Date().getMonth()+1;
 
-        // Form Validation - Step 4
+        if (checkYear > expYear) {
+            throw new Error("Card is Expired or Incorrect")
+        } else if (expYear === currentYear && checkMonth > expMon) {
+            throw new Error("Card is Expired or Incorrect")
+        }
+            
+        // Form Validation - Step 4 -- CCV Length Check
+        const CCVlen = CCV.toString().length;
+        if (2 >= CCVlen || CCVlen >= 5) {
+            throw new Error("CCV Incorrect Length")
+        }
 
         // Compile to Send
         const sendData = {
@@ -69,6 +85,7 @@ document.getElementById("pay-form"). addEventListener("submit", async function(e
             "cvv_code": CCV
         }
 
+        // Post Data
         const response = await fetch("https://mudfoot.doc.stu.mmu.ac.uk/node/api/creditcard", {
             method: "POST",
             headers: {
@@ -77,9 +94,10 @@ document.getElementById("pay-form"). addEventListener("submit", async function(e
             body: JSON.stringify(sendData)
             
         })
+        // Await and store Response
         const res = await response.json();
 
-        alert(res);
+        alert(res.message);
     }
     catch(error) {
         const errortxt = document.getElementById("error-text")
